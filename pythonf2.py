c12f3b1218d27eae5b99c7abb36cbd48d28440bf
shilpa <92202761+Chaitrashilpa@users.noreply.github.com>
2023-03-03 14:03:04 +0530

    Update pythonf2.py

--- pythonf2.py
+++ pythonf2.py
@@ -1,1 +1,14 @@
-print("hello world")
+a = 7
+b = 2
+
+# addition
+print ("Sum: ", a + b)  
+
+# subtraction
+print ("Subtraction: ", a - b)   
+
+# multiplication
+print ("Multiplication: ", a * b)  
+
+# division
+print ("Division: ", a / b) 
