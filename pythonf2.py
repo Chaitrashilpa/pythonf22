6e1060af3082e9fb49c24a75ed0d8f769a72847a
shilpa <92202761+Chaitrashilpa@users.noreply.github.com>
2023-03-03 14:04:06 +0530

    Update pythonf2.py

--- pythonf2.py
+++ pythonf2.py
@@ -1,14 +1,10 @@
 a = 7
 b = 2
 
-# addition
 print ("Sum: ", a + b)  
 
-# subtraction
 print ("Subtraction: ", a - b)   
 
-# multiplication
 print ("Multiplication: ", a * b)  
 
-# division
 print ("Division: ", a / b) 
